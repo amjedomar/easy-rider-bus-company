45472210c9ce90cbb477aa56d03cc7bf73103885
Amjed Omar <40240660+amjedomar@users.noreply.github.com>
2023-01-17 00:15:38 +0200

    complete: Stage 6/6: On-demand

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import json
 
 rides = json.loads(input())
 
-wrong_buses = []
 
-print('Arrival time test:')
+def count_demand(stop_id):
+    total = 0
+    for ride in rides:
+        if ride['next_stop'] == stop_id:
+            total += 1
+    return total
 
-for rideI, ride in enumerate(rides):
-    prev_ride = rides[rideI - 1] if rideI > 0 else None
 
-    is_invalid = ride['stop_type'] != 'S' and prev_ride['bus_id'] == ride['bus_id'] and \
-                 ride['bus_id'] not in wrong_buses and prev_ride['a_time'] >= ride['a_time']
+def main():
+    print('On demand stops test:')
 
-    if is_invalid:
-        print(f"bus_id line {ride['bus_id']}: wrong time on station {ride['stop_name']}")
-        wrong_buses.append(ride['bus_id'])
+    demands = []
 
-if len(wrong_buses) == 0:
-    print('OK')
+    for ride in rides:
+        if ride['stop_type'] == 'O':
+            demand_num = count_demand(ride['stop_id'])
+
+            if demand_num > 1 and ride['stop_name'] not in demands:
+                demands.append(ride['stop_name'])
+
+    if demands:
+        print('Wrong stop type:', sorted(demands))
+    else:
+        print('OK')
+
+
+main()
