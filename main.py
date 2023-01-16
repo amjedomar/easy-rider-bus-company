dba121784f768eaeea9f98577fd2ee3ec7e5c472
Amjed Omar <40240660+amjedomar@users.noreply.github.com>
2023-01-17 00:13:52 +0200

    complete: Stage 5/6: Unlost in time

--- main.py
+++ main.py
@@ -2,43 +2,19 @@ import json
 
 rides = json.loads(input())
 
-def has_stop(match_val, stop_type):
-    for ride in rides:
-        if ride['bus_id'] == match_val and ride['stop_type'] == stop_type:
-            return True
-    return False
+wrong_buses = []
 
+print('Arrival time test:')
 
-def stop_count(stop_name):
-    count = 0
-    for ride in rides:
-        if ride['stop_name'] == stop_name:
-            count += 1
-    return count
+for rideI, ride in enumerate(rides):
+    prev_ride = rides[rideI - 1] if rideI > 0 else None
 
+    is_invalid = ride['stop_type'] != 'S' and prev_ride['bus_id'] == ride['bus_id'] and \
+                 ride['bus_id'] not in wrong_buses and prev_ride['a_time'] >= ride['a_time']
 
-def main():
-    starts, transfers, finishes = [[] for _ in range(0, 3)]
+    if is_invalid:
+        print(f"bus_id line {ride['bus_id']}: wrong time on station {ride['stop_name']}")
+        wrong_buses.append(ride['bus_id'])
 
-    for ride in rides:
-        bus_id = ride['bus_id']
-        stop_type = ride['stop_type']
-        stop_name = ride['stop_name']
-
-        if stop_type == 'S' and stop_name not in starts:
-            if not has_stop(bus_id, 'F'):
-                return print(f'There is no start or end stop for the line: {bus_id}.')
-            starts.append(stop_name)
-        elif stop_type == 'F' and stop_name not in finishes:
-            if not has_stop(bus_id, 'S'):
-                return print(f'There is no start or end stop for the line: {bus_id}.')
-            finishes.append(stop_name)
-        elif stop_name not in transfers and stop_count(stop_name) > 1:
-            transfers.append(stop_name)
-
-    print('Start stops:', len(starts), sorted(starts))
-    print('Transfer stops:', len(transfers), sorted(transfers))
-    print('Finish stops:', len(finishes), sorted(finishes))
-
-
-main()
+if len(wrong_buses) == 0:
+    print('OK')
