1a01c824600a4271133a47172de7270cd9e01f2f
Amjed Omar <40240660+amjedomar@users.noreply.github.com>
2023-01-17 00:13:15 +0200

    complete: Stage 4/6: Special stops

--- main.py
+++ main.py
@@ -1,51 +1,44 @@
 import json
-# import re
 
-# input
 rides = json.loads(input())
 
-# validation
-# properties = {
-#     'stop_name': {
-#         'regex': r'[A-Z].+ (Road|Avenue|Boulevard|Street)$',
-#     },
-#     'stop_type': {
-#         'regex': '[SOF]{,1}$'
-#     },
-#     'a_time': {
-#         'regex': '(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$'
-#     }
-# }
-#
-# errors = {propKey: 0 for propKey in properties}
-#
-# for ride in rides:
-#     for propKey in ride:
-#         if propKey in properties:
-#             val = ride[propKey]
-#             rules = properties[propKey]
-#
-#             ruleRegex = rules.get('regex')
-#
-#             is_invalid = ruleRegex and re.match(ruleRegex, val) is None
-#
-#             if is_invalid:
-#                 errors[propKey] += 1
-#
-# errorsNum = sum(errors.values())
-# print(f'Type and required field validation: {errorsNum} errors')
-# for errKey in errors:
-#     errorNum = errors[errKey]
-#     print(f'{errKey}: {str(errorNum)}')
+def has_stop(match_val, stop_type):
+    for ride in rides:
+        if ride['bus_id'] == match_val and ride['stop_type'] == stop_type:
+            return True
+    return False
 
-# stops
-stops = {}
 
-print('Line names and number of stops:')
+def stop_count(stop_name):
+    count = 0
+    for ride in rides:
+        if ride['stop_name'] == stop_name:
+            count += 1
+    return count
 
-for ride in rides:
-    bus_id = ride['bus_id']
-    stops[bus_id] = stops.get(bus_id, 0) + 1
 
-for bus_id in stops:
-    print(f"bus_id: {bus_id}, stops: {stops[bus_id]}")
+def main():
+    starts, transfers, finishes = [[] for _ in range(0, 3)]
+
+    for ride in rides:
+        bus_id = ride['bus_id']
+        stop_type = ride['stop_type']
+        stop_name = ride['stop_name']
+
+        if stop_type == 'S' and stop_name not in starts:
+            if not has_stop(bus_id, 'F'):
+                return print(f'There is no start or end stop for the line: {bus_id}.')
+            starts.append(stop_name)
+        elif stop_type == 'F' and stop_name not in finishes:
+            if not has_stop(bus_id, 'S'):
+                return print(f'There is no start or end stop for the line: {bus_id}.')
+            finishes.append(stop_name)
+        elif stop_name not in transfers and stop_count(stop_name) > 1:
+            transfers.append(stop_name)
+
+    print('Start stops:', len(starts), sorted(starts))
+    print('Transfer stops:', len(transfers), sorted(transfers))
+    print('Finish stops:', len(finishes), sorted(finishes))
+
+
+main()
